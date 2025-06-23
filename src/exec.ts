76464d03032cdd79c9aecca3584f3ded8877ad50
chi <chi@taiwan.im>
2025-06-23 17:13:24 +0700

    refactor: remove exec.ts

--- src/exec.ts
+++
@@ -1,9 +0,0 @@
-import { buy } from "./buy";
-import type { Env } from "./Env";
-import { sendTelegramMessage } from "./sendTelegramMessage";
-
-export const exec = async (env: Env) => {
-  const message = await buy(env.MAX_API_KEY, env.MAX_API_SECRET);
-
-  await sendTelegramMessage(env.TELEGRAM_BOT_TOKEN, env.TELEGRAM_CHAT_ID, message);
-}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import dotenv from 'dotenv';
-import { exec } from './exec';
 import { resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import type { Env } from './Env';
+import { buy } from './buy';
+import { sendTelegramMessage } from './sendTelegramMessage';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = resolve(__filename, '..');
 const envPath = resolve(__dirname, '../.env');
+const env = dotenv.config({ path: envPath }).parsed as unknown as Env
 
-await exec(dotenv.config({ path: envPath }).parsed as unknown as Env)
+const message = await buy(env.MAX_API_KEY, env.MAX_API_SECRET);
+
+await sendTelegramMessage(env.TELEGRAM_BOT_TOKEN, env.TELEGRAM_CHAT_ID, message);
