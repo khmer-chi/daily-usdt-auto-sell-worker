21a316e119eee938a62ac9df349f8cc1735c03a6
chi <chi@taiwan.im>
2025-06-23 16:02:13 +0700

    update

--- src/run.ts
+++ src/run.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import { exec } from './exec';
 import { resolve } from 'path';
 import { fileURLToPath } from 'url';
+console.log(123)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = resolve(__filename, '..');
 const envPath = resolve(__dirname, '../.dev.vars');
