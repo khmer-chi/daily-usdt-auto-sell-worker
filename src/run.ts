79ad8c04c4f4c507e3b68262e4c573c8e2f1cc1d
chi <chi@taiwan.im>
2025-06-23 16:57:59 +0700

    update

--- src/run.ts
+++ src/run.ts
@@ -2,9 +2,9 @@ import dotenv from 'dotenv';
 import { exec } from './exec';
 import { resolve } from 'path';
 import { fileURLToPath } from 'url';
-console.log(123)
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = resolve(__filename, '..');
 const envPath = resolve(__dirname, '../.dev.vars');
-console.log({ envPath })
+
 await exec(dotenv.config({ path: envPath }).parsed as any as Env)
